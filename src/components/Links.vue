c763045b9fb389eb4b170131eb108efcb4f9cd23
geyu <geyu@zuche.com>
2024-06-21 13:53:48 +0800

    更新Links

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, CalendarCheck, Tasks} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, React, LaptopCode, CalendarCheck, Tasks} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -68,8 +68,11 @@ const siteIcon = {
   CompactDisc,
   Compass,
   Book,
+  React,
   Fire,
   LaptopCode,
+  CalendarCheck,
+  Tasks
 };
 
 // 链接跳转
